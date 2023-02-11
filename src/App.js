488746bd84ead9375492db6730ed72b29eb2bc24
thomas <thomas@Ubuntu.myguest.virtualbox.org>
2023-02-11 19:49:35 +0800

    Try fixing clien side rounting issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as HashRouter, Router, Routes, Route, Navigate} from 'react-router-dom';
+import {Router, Routes, Route, Navigate} from 'react-router-dom';
 import Home from "./Pages/Home";
 import Projects from "./Pages/Projects";
 import Experience from "./Pages/Experience";
@@ -10,7 +10,7 @@ import ProjectDisplay from './Pages/ProjectDisplay';
 function App() {
   return (
     <div className="App">
-      <HashRouter basename="/">  
+      <Router basename="/">  
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -20,7 +20,7 @@ function App() {
           <Route path="/website" element={ <Navigate to="/" /> } />
         </Routes>
         <Footer />
-      </HashRouter>
+      </Router>
     </div>
     );
 }

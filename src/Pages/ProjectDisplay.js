b8352350a8a6165ebb09b65b85ca69536d2bfe38
thomas <thomas@Ubuntu.myguest.virtualbox.org>
2023-02-14 06:54:44 +0800

    test animaion

--- src/Pages/ProjectDisplay.js
+++ src/Pages/ProjectDisplay.js
@@ -17,7 +17,7 @@ function ProjectDisplay() {
             className="project"
             initial={{ opacity: 0, transition: {duration: 1}}}
             animate={{ opacity: 1, transition: {duration: 1} }}
-            exit={{opacity: 0, transition: {duration: 1}}}
+            exit={{opacity: 0, transition: {duration: 0.5}}}
         >
             <div className="heading">
                 <h1> {project.name} </h1>

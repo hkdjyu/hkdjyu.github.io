7852ee098bdd9e8e3b2e8a6e695108fa366103c1
thomas <thomas@Ubuntu.myguest.virtualbox.org>
2023-02-14 06:50:54 +0800

    fixed animation

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,9 +14,9 @@ function Home() {
   return (
     <motion.div 
       className="home"
-      initial={{ opacity: 0,}}
+      initial={{ opacity: 0, transition: {duration: 1}}}
       animate={{ opacity: 1, transition: {duration: 1} }}
-      exit={{opacity: 0, transition: {duration: 0.5}}}
+      exit={{opacity: 0, transition: {duration: 1}}}
     >
       <div className="about">
         <div className="propic"><
--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -8,8 +8,8 @@ function Projects() {
   return (
     <motion.div 
       className="projects"
-      initial={{opacity: 0}}
-      animate={{opacity: 1, }}
+      initial={{ opacity: 0, transition: {duration: 1}}}
+      animate={{ opacity: 1, transition: {duration: 1} }}
       exit={{opacity: 0, transition: {duration: 1}}}
     >
       <div className="projectsHeader">
